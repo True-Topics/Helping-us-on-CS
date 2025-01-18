2b25c6ef35cc979ecb5f29331bd1d8e408ebf097
quantlight <shubhmistry0@gmail.com>
2025-01-18 15:14:17 +0530

    Quartz sync: Jan 18, 2025, 3:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Poppins",
-        body: "Source Sans Pro",
+        body: "Poppins",
         code: "IBM Plex Mono",
       },
       colors: {
